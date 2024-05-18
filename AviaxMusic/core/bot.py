c0c1e44e02a33ddd01a048e20cf3e4ebc6b0cd0c
blankchit <156068126+blankchit@users.noreply.github.com>
2024-05-18 14:33:57 +0530

    Update bot.py

--- AviaxMusic/core/bot.py
+++ AviaxMusic/core/bot.py
@@ -10,7 +10,7 @@ class Aviax(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="AviaxMusic",
+            name="BlankchitMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
